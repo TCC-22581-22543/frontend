8629938c9ef3cd576579163928b6498b7a820db5
unknown <matheus.hchiarelli@gmail.com>
2023-09-08 13:06:06 -0300

    import changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-  };
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          "@assets": "./src/Components",
+          "@components": "./src/Components",
+          "@routes": "./src/Routes",
+          "@Screens": "./src/Screens",
+          "@storage": "./src/Storage",
+          "@theme": "./src/Theme",
+          "@utils": "./src/Utils"
+        }
+      }]
+    ]
+  }
 };
---
+++ src/Screens/Login/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Login(){
+    return(
+        <View style={styles.container}>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
